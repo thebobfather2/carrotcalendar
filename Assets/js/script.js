8d4072b44fd8fb0657a2467d2edacc0383090cd1
Anastasiia Markova <anastasia19markova@gmail.com>
2022-12-04 22:01:41 -0500

    added time

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -245,16 +245,6 @@ function renderWeek5() {
     }
 };
 
-// Show and hide dropdown list item on button click
-function show_hide() {
-    var click = document.getElementById("list-items");
-    if (click.style.display === "none") {
-        click.style.display = "block";
-    } else {
-        click.style.display = "none";
-    }
-}
-
 
 
 
