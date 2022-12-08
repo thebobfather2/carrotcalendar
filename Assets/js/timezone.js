91eecaa07edfed67fa3bdf8eb3f614376c630832
Sarah Jacobs <sgjacobs14@gmail.com>
2022-12-08 18:17:57 -0500

    added dropdown menu

--- Assets/js/timezone.js
+++ Assets/js/timezone.js
@@ -1,11 +1,15 @@
 
-const timezoneDiv = document.getElementById('#timezone')
-const country = document.getElementById('country')
+// const timezoneDiv = document.getElementById('#timezone')
+// const country = document.getElementById('country')
+let city = "America/New_York"
 
-//fetching the timezone apo
-const timezone = () => {
-    fetch("http://worldtimeapi.org/api/timezone/America/New_York")
-    //storing the response, then pulling the datetime to display
+
+//fetching the timezone api
+const timezone = (city) => {
+    var timezoneApi = "http://worldtimeapi.org/api/timezone/" + city 
+
+    fetch(timezoneApi)
+        //storing the response, then pulling the datetime to display
         .then(response => response.json())
         .then(json => {
             console.log(json)
@@ -13,15 +17,42 @@ const timezone = () => {
             console.log(json.datetime)
             //need to somehow shorten the response so it doesn't print the entire return
             //need to build a prompt for users to choose their timezone, then a new function will switch the timezones with interpolation
-            
+
             let jsonString = json.datetime
-            time = console.log(new Date(jsonString).toLocaleString('en-US', {hour12: false}))
+            time = console.log(new Date(jsonString).toLocaleString('en-US', { hour12: false }))
 
 
             //below currently prints JUST the second json, not both. It also prints the long date, not the hour
-            document.getElementById('timeDisplay').innerText = new Date(jsonString).toLocaleString('en-US', {hour12: false})
+            document.getElementById('timeDisplay').innerText = new Date(jsonString).toLocaleString('en-US', { hour12: false })
 
-            
-        })}
-        timezone()
 
+        })
+}
+
+estFunction = (city) => {
+    timezone("America/New_York")
+    console.log(timezone("America/New_York"))
+    
+}
+
+cstFunction = (city) => {
+    timezone("America/New_York")
+    console.log(timezone("America/New_York"))
+    
+}
+
+pstFunction = (city) => {
+    timezone("America/Los_Angeles")
+    
+}
+
+jpFunction = (city) => {
+    timezone("Asia/Tokyo")    
+}
+
+eurFunction = (city) => {
+    timezone("America/New_York")
+    console.log(timezone("America/New_York"))
+    
+}
+estFunction();
